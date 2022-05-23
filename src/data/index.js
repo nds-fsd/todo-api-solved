07360fac77cdd6087f744179968d45161ddd204a
Jose Manuel Cano <jmcano94@gmail.com>
2022-05-23 16:26:26 +0200

    TODO router done

---
+++ src/data/index.js
@@ -0,0 +1,20 @@
+//Este fichero actuará de fuente de datos para nuestro proyecto por elmomento. Conforme vayamos necesitando más recuros,
+// los iremos añadiendo aqui y los exportaremos para poder utilizarlos en los routers que hagan falta.
+
+
+exports.todos = [{
+  id: 0,
+  text: "Hacer la cama",
+  fecha: new Date("2022-05-21"),
+  done: false
+},
+{ 
+  id: 1,
+  text: "Ir al super",
+  fecha: new Date("2022-05-14"),
+  done: true
+}];
+
+
+
+
